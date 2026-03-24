712bea95b28a80812c89f7680f656bd3e336314d
Muhammad Ali <muhammad.ali.aktz@gmail.com>
2026-03-23 22:29:42 -0400

    update

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print()
+import random
+empty_dict = dict()
+
+sides = int(input("Enter number of sides: "))
+rolls = int(input("Enter number of rolls: "))
+total = 0
+for i in range(rolls):
+    random_integer = random.randint(1, sides)
+    total+=random_integer
+    if random_integer in empty_dict:
+        empty_dict[random_integer]+=1
+    else:
+        empty_dict[random_integer] =1
+
+
